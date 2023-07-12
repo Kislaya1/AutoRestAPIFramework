790ef3dea4cdf1e80c197e614356501db90cab85
Kislaya1 <kislayapant@gmail.com>
2023-07-12 22:10:34 +0530

    Refractored Code

--- src/test/java/com/auto/rest/api/enums/SchemaFileLocations.java
+++ src/test/java/com/auto/rest/api/enums/SchemaFileLocations.java
@@ -4,10 +4,10 @@ public enum SchemaFileLocations {
   BOOKING_DATA_SCHEMA("booking-data-schema.json"),
   BOOKING_SCHEMA("booking-schema.json"),
   GET_ALL_BOOKING_SCHEMA("get-all-booking-schema.json");
-  private final String basePath = "src/test/java/com/auto/rest/api/schemas/booking/";
-  private String filePath;
+  private final String filePath;
 
   SchemaFileLocations(final String filePath) {
+    final String basePath = "src/test/java/com/auto/rest/api/schemas/booking/";
     this.filePath = basePath + filePath;
   }
 
